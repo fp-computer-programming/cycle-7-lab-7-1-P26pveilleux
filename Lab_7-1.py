aa53f32724f7fe4d22f3fffdcb58c3d2b3f6975f
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-11-14 08:56:10 -0500

    done

---
+++ Lab_7-1.py
@@ -0,0 +1,15 @@
+
+#Create a Python file named lab_7-1.py
+
+#*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+#Create a function called greeting()
+def greeting():
+#Add a docstring to the function that explains how the function can only print "Hello World" on one line
+    '''this function can only print the phrase hello world'''
+#Add a statement in the function to print "Hello World!"
+    print ("hello world")
+#Add a statement that returns the docstring for the greeting function
+help (greeting) 
+#Add another statement to call the functio
+greeting()
